fab7b8caf3a8c4041c46cfccc0381cf4fd1c8835
Safiy Cham <csafiy@yahoo.com>
2019-04-08 10:08:24 -0700

    Split code into functional components

---
+++ src/components/Ticket.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Ticket() {
+	return (
+		<div>
+			<h3>2a</h3>
+			<h3>Thato and Haley</h3>
+			<p>
+				<em>Firebase will not save record!</em>
+			</p>
+			<hr />
+		</div>
+	);
+}
+
+export default Ticket;
