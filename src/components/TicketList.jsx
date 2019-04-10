1ac10e5a9da8714825065ac68d2e0b5b37ac6128
Safiy Cham <csafiy@yahoo.com>
2019-04-10 11:37:25 -0700

    Incorporate dev server and hot reloading

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Ticket from './Ticket';
 
 function TicketList() {
-	return <Ticket />;
+	return <Ticket location="3A" names="Thato and Haley" issue="Firebase will not save record!" />;
 }
 
 export default TicketList;
